a03028cf72bea271c84c81a93b30cb4a1090a73c
Ykulkarni-ops <kulkarniyash333@gmail.com>
2021-10-24 16:39:47 -0400

    Update: Changes in main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -8,23 +8,29 @@
 ///============================================================================
 
 #include <ik.h>
+#include <GL/glut.h>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
 
-/// @fn int main()
-/// @brief The main function is to run and execute all the functions
-/// @return 0
-int main() {
-  /*Ik_solver rob;
-   Currently commented for coveralls
-   double x, y, z,phi;
-   rob.set_initial();
-   std::cout << "Enter the final position and end effector angle" << std::endl;
-   std::cin >> x;
-   std::cin >> y;
-   std::cin >> z;
-   std::cin >> phi;
-   rob.input_end_point(x, y, z);
-   rob.ik_solve(phi);
-   */
-  return 0;
+using namespace std;
+Ik_solver rob;
+GLfloat cameraDistance = 12, cameraAngle = 0;
+int c = 0;
+// angles = { Y rotation at shoulder, Z rotation at shoulder, Z rotation at elbow, X rotation at wrist }
+int arm_angles[5] = { 0, 0, 0, 0, 0 };
+enum {
+  SHOULDER_Y,
+  SHOULDER_Z,
+  ELBOW_Z,
+  WRIST_X,
+  WRIST_Z
+};
+
+void change_angle(int angle, int delta, int minimum = 0, int maximum = 180) {
+  arm_angles[angle] = (arm_angles[angle] + delta) % 360;
+  arm_angles[angle] = max(arm_angles[angle], minimum);
+  arm_angles[angle] = min(arm_angles[angle], maximum);
 }
